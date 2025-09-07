f9cd4154cec2c7655c7aa4fc9e1874ad79f66116
huangjinxian <1549892116@qq.com>
2025-09-07 21:36:25 +0800

    feat: add convert cli flags.

--- cmd/book_convert.go
+++ cmd/book_convert.go
@@ -8,6 +8,14 @@ import (
 )
 
 var (
+	title           string
+	author          string
+	coverPath       string
+	chapterTitleReg string
+	fileEncoding    string
+	filePath        string
+	destPath        string
+
 	port int16
 )
 
@@ -18,18 +26,40 @@ var rootCmd = &cobra.Command{
 Support book type includes txt, epub and azw3.`,
 }
 
+var convertCmd = &cobra.Command{
+	Use:   "convert",
+	Short: "Convert txt book to epub book",
+	Long:  `Convert txt book to epub book.`,
+	Args:  cobra.MinimumNArgs(1),
+	Run:   convert,
+}
+
 var webCmd = &cobra.Command{
 	Use:   "web [-p port]",
 	Short: "start web server",
-	Long:  ``,
+	Long:  `#todo need to implement the webServer func`,
 	Args:  cobra.MinimumNArgs(1),
 	Run:   webServer,
 }
 
 func init() {
 
+	convertCmd.Flags().StringVarP(&title, "title", "t", "", "book title")
+	convertCmd.Flags().StringVarP(&author, "author", "a", "", "book author")
+	convertCmd.Flags().StringVarP(&coverPath, "cover", "p", "", "cover path")
+	convertCmd.Flags().StringVarP(&chapterTitleReg, "chapter", "r", "", "chapter title regular expression")
+	convertCmd.Flags().StringVarP(&fileEncoding, "encoding", "e", "GB18030", "encoding type")
+	convertCmd.Flags().StringVarP(&filePath, "file", "f", "", "file path")
+	convertCmd.Flags().StringVarP(&destPath, "output", "o", "", "output directory")
+
 	webCmd.Flags().Int16VarP(&port, "port", "p", 8080, "Port to listen on")
+
+	rootCmd.AddCommand(convertCmd)
 	rootCmd.AddCommand(webCmd)
+}
+
+func convert(cmd *cobra.Command, args []string) {
+	fmt.Println(title, author, coverPath, chapterTitleReg, fileEncoding, filePath, destPath)
 }
 
 func webServer(cmd *cobra.Command, args []string) {
